e17202c82201bb14a46acc6a909fb73b716c1b63
Maruf <maruf02@student.sust.edu>
2020-01-23 16:01:35 +0600

    Can store message in slack

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SlackBotMessages;
+using SlackBotMessages.Models;
+
+namespace SlackMessaging
+{
+    class Program
+    {
+        private static string WebHookUrl;
+
+        static void Main(string[] args)
+        {
+            var client = new SbmClient("https://hooks.slack.com/services/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");
+
+            var message = new Message
+            {
+                //{ Console.WriteLine("Enter integer") },
+                Username = Console.ReadLine(),
+                //Text = Console.ReadLine(),
+                Text = Console.ReadLine(),
+                IconEmoji = Emoji.Alien,
+            };
+
+            client.Send(message);
+
+            //SBMClient client = new SBMClient("https://hooks.slack.com/services/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");
+        }
+
+    }
+}
